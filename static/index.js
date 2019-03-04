eec13beb505818e9b07c17cb5b7d9714387c744a
Jarrett Kong <jarrettkong@me.com>
2019-03-04 10:39:12 -0700

    Adjusted card sizes and fixed repeated event listeners

--- static/index.js
+++ static/index.js
@@ -1,38 +1,45 @@
+let socket = null
+
 const medCode = document.querySelector('h2 span')
 const quizOptions = document.getElementsByClassName('quiz-option')
+
+const listeners = {};
 
 function displayQuiz(dataset) {
   medCode.innerText = dataset.code
   for(let i = 0; i < quizOptions.length; i++) {
-    quizOptions[i].innerText = dataset.options[i];
+    quizOptions[i].innerText = dataset.options[i]
   }
 }
 
-function newQuiz(dataset, callback) {
+function newQuiz(dataset, listeners, callback) {
   displayQuiz(dataset)
   Array.from(quizOptions).forEach(option => {
-    option.addEventListener('click', checkAnswer(dataset, dataset.options.indexOf(option.innerText), callback));
-  });
+    option.removeEventListener('click', listeners[option.dataset.id])
+    const listener = checkAnswer(dataset, option, callback)
+    option.addEventListener('click', listener)
+    listeners[option.dataset.id] = listener
+  })
 }
 
-function checkAnswer(dataset, index, callback) {
+function checkAnswer(dataset, option, callback) {
   return function(e) {
-    if (index === dataset.answer) {
-      alert('correct');
+    const optionIndex = dataset.options.indexOf(option.innerText)
+    if (optionIndex === dataset.answer) {
+      alert('correct')
       callback()
-    }
-   else {
-      alert('wrong');
+    } else {
+      alert('wrong')
     }
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const socket = io()
+  socket = io()
 
   socket.emit('newGame')
 
   socket.on('game', game => {
-    newQuiz(game, () => socket.emit('newGame'))
+    newQuiz(game, listeners, () => socket.emit('newGame'))
   })
 })
